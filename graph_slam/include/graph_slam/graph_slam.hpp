81ca764b06c7a8eb919c44c27ade3c8585c3284c
TomasMSantos <tomasmarcelinosantos@gmail.com>
2026-01-18 20:52:47 +0000

    Added main graph slam files

---
+++ graph_slam/include/graph_slam/graph_slam.hpp
@@ -0,0 +1,26 @@
+#ifndef GRAPH_SLAM_H_
+#define GRAPH_SLAM_H_
+
+#include "rclcpp/rclcpp.hpp"
+#include "associationSolver.hpp"
+#include "lart_msgs/msg/cone_array.hpp"
+
+#define ASSICIATION_MODE 1
+#define CONES_TOPIC "/mapping/cones"
+
+class GraphSLAM : public rclcpp::Node
+{
+public:
+    GraphSLAM();
+    ~GraphSLAM();
+
+    void observations_callback(const lart_msgs::msg::ConeArray::SharedPtr msg);
+
+private:
+    rclcpp::Subscription<lart_msgs::msg::ConeArray>::SharedPtr observations_subscriber_;
+
+protected:
+    AssociationSolver *association_solver_;
+};
+
+#endif
---
+++ graph_slam/src/graph_slam.cpp
@@ -0,0 +1,35 @@
+#include "graph_slam/graph_slam.hpp"
+
+using std::placeholders::_1;
+
+GraphSLAM::GraphSLAM() : Node("graph_slam_node")
+{
+    RCLCPP_INFO(this->get_logger(), "GraphSLAM node has been started.");
+
+    association_solver_ = new AssociationSolver(ASSICIATION_MODE);
+
+    observations_subscriber_ = this->create_subscription<lart_msgs::msg::ConeArray>(
+        CONES_TOPIC, 10,
+        std::bind(&GraphSLAM::observations_callback, this, _1));
+}
+
+GraphSLAM::~GraphSLAM()
+{
+    delete association_solver_;
+    RCLCPP_INFO(this->get_logger(), "GraphSLAM node has been terminated.");
+}
+
+void GraphSLAM::observations_callback(const lart_msgs::msg::ConeArray::SharedPtr msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received ConeArray with %zu cones.", msg->cones.size());
+
+    association_solver_->associate(msg->cones);
+}
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GraphSLAM>());
+    rclcpp::shutdown();
+    return 0;
+}
