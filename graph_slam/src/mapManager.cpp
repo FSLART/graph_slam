a302009dece6e94ec39098345c1431284a44a579
André Lopes <andreglopes2005@gmail.com>
2026-03-16 15:24:47 +0000

    starting map loader implementation

---
+++ graph_slam/src/mapManager.cpp
@@ -0,0 +1,15 @@
+#include "graph_slam/mapManager.hpp"
+
+MapManager::MapManager()
+{
+}
+
+void MapManager::load_map(const std::string& filename)
+{
+    // Implement map loading logic here
+}
+
+void MapManager::save_map(const std::string& filename)
+{
+    // Implement map saving logic here
+}
